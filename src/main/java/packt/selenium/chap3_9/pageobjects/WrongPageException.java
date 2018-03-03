90ccc1ca6c721e06294cc249087dce9e8654664c
Nenad Erceg <nenaderceg77@gmail.com>
2018-03-03 15:47:58 -0500

    driver exe moved

--- src/main/java/packt/selenium/chap3_9/pageobjects/WrongPageException.java
+++ src/main/java/packt/selenium/chap3_9/pageobjects/WrongPageException.java
@@ -4,7 +4,12 @@ package packt.selenium.chap3_9.pageobjects;
  * Created by Ripon on 11/19/2015.
  */
 public class WrongPageException extends RuntimeException{
-    public WrongPageException(String message){
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public WrongPageException(String message){
         super(message);
     }
 }
